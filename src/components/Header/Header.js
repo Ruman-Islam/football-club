454d398ab358607182b2efb7aed549f98483c29b
Ruman-Islam <rumanislam48@gmail.com>
2021-03-13 02:26:17 +0600

    fixed err

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../Photo/premierLogo.png';
-import HomeIcon from '@material-ui/icons/AccessAlarms';
+import HomeRounded from '@material-ui/icons/HomeRounded';
 
 const Header = () => {
     return (
@@ -12,7 +12,7 @@ const Header = () => {
             <Navbar bg="primary" variant="dark">
                 <Navbar.Brand><Link to="/home"><img src={logo} alt=""/></Link></Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link> <Link to="/"><HomeIcon></HomeIcon></Link></Nav.Link>
+                    <Nav.Link> <Link to="/"><HomeRounded style={{fontSize:'50px', color: 'white'}}></HomeRounded></Link></Nav.Link>
                 </Nav>
             </Navbar>
         </div>
